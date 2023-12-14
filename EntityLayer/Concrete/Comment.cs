e0b9e538660f78947cc7656b6cf3dc2f84bec654
msayrac <msayracode@gmail.com>
2023-12-14 20:16:59 +0300

    Category and Blog category  are created

--- EntityLayer/Concrete/Comment.cs
+++ EntityLayer/Concrete/Comment.cs
@@ -17,5 +17,9 @@ namespace EntityLayer.Concrete
         public DateTime CommentDate { get; set; }
         public bool CommentStatus { get; set; }
 
+        public int BlogID { get; set; }
+        public Blog Blog { get; set; }
     }
+
+
 }
