ae8f09f6d37a5ceba183e75625e996098939c157
msayrac <msayracode@gmail.com>
2023-12-15 14:17:53 +0300

    Controller is defined

---
+++ BusinessLayer/Concrete/CategoryManager.cs
@@ -0,0 +1,45 @@
+﻿using BusinessLayer.Abstract;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+
+namespace BusinessLayer.Concrete
+{
+    public class CategoryManager : ICategoryService
+    {
+        EfCategoryRepository efCategoryRepository;
+
+        public CategoryManager()
+        {
+            efCategoryRepository = new EfCategoryRepository();
+
+        }
+
+
+        public void CategoryAdd(Category category)
+        {
+
+            efCategoryRepository.Insert(category);
+
+        }
+
+        public void CategoryDelete(Category category)
+        {
+            efCategoryRepository.Delete(category);
+        }
+
+        public void CategoryUpdate(Category category)
+        {
+           efCategoryRepository.Update(category);
+        }
+
+        public Category GetById(int id)
+        {
+            return efCategoryRepository.GetById(id);
+        }
+
+        public List<Category> GetList()
+        {
+            return efCategoryRepository.GetListAll();
+        }
+    }
+}
