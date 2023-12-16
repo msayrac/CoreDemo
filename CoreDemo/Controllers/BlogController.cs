23e9d18f698144830c98f6208b5bed2e225e5371
msayrac <msayracode@gmail.com>
2023-12-16 13:57:30 +0300

    blog page created

---
+++ CoreDemo/Controllers/BlogController.cs
@@ -0,0 +1,19 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreDemo.Controllers
+{
+    public class BlogController : Controller
+    {
+        BlogManager bm = new BlogManager(new EfBlogRepository());
+
+        public IActionResult Index()
+        {
+            var values = bm.GetList();
+
+			return View(values);
+        }
+    }
+}
