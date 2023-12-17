f98c1ec6811797ac272082cc0f5c7956b9172ced
msayrac <msayracode@gmail.com>
2023-12-17 21:15:50 +0300

    comment layer added

--- BusinessLayer/Concrete/CategoryManager.cs
+++ BusinessLayer/Concrete/CategoryManager.cs
@@ -1,46 +1,43 @@
 ﻿using BusinessLayer.Abstract;
 using DataAccessLayer.Abstract;
-using DataAccessLayer.EntityFramework;
 using EntityLayer.Concrete;
 
 namespace BusinessLayer.Concrete
 {
-    public class CategoryManager : ICategoryService
-    {
-        ICategoryDal _categoryDal;
+	public class CategoryManager : ICategoryService
+	{
+		ICategoryDal _categoryDal;
 
-              
-        public CategoryManager(ICategoryDal categoryDal)
-        {
-            _categoryDal = categoryDal;
-        }
 
-        public void CategoryAdd(Category category)
-        {
+		public CategoryManager(ICategoryDal categoryDal)
+		{
+			_categoryDal = categoryDal;
+		}
 
-            _categoryDal.Insert(category);
-            
+		public void CategoryAdd(Category category)
+		{
+			_categoryDal.Insert(category);
 
-        }
+		}
 
-        public void CategoryDelete(Category category)
-        {
-            _categoryDal.Delete(category);
-        }
+		public void CategoryDelete(Category category)
+		{
+			_categoryDal.Delete(category);
+		}
 
-        public void CategoryUpdate(Category category)
-        {
-            _categoryDal.Update(category);
-        }
+		public void CategoryUpdate(Category category)
+		{
+			_categoryDal.Update(category);
+		}
 
-        public Category GetById(int id)
-        {
-            return _categoryDal.GetById(id);
-        }
+		public Category GetById(int id)
+		{
+			return _categoryDal.GetById(id);
+		}
 
-        public List<Category> GetList()
-        {
-            return _categoryDal.GetListAll();
-        }
-    }
+		public List<Category> GetList()
+		{
+			return _categoryDal.GetListAll();
+		}
+	}
 }
---
+++ CoreDemo/ViewComponents/CommentList.cs
@@ -0,0 +1,33 @@
+﻿using CoreDemo.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreDemo.ViewComponents
+{
+	public class CommentList : ViewComponent
+	{
+		public IViewComponentResult Invoke()
+		{
+			var commentValues = new List<UserComment>
+			{
+				new UserComment
+				{
+					ID=1,
+					Username="Murat"
+				},
+				new UserComment
+				{
+					ID=2,
+					Username = "Mesut"
+				},
+				new UserComment
+				{
+					ID=3,
+					Username="Merve"
+				}
+
+			};
+			return View(commentValues);
+		}
+
+	}
+}
