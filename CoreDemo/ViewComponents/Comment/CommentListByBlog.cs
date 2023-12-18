84e683b0d23951cb736c61d4e4ac8fc1f9a67f25
msayrac <msayracode@gmail.com>
2023-12-18 18:42:09 +0300

    comment list added

---
+++ CoreDemo/ViewComponents/Comment/CommentListByBlog.cs
@@ -0,0 +1,20 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreDemo.ViewComponents.Comment
+{
+	public class CommentListByBlog:ViewComponent
+	{
+		CommentManager cm = new CommentManager(new EfCommentRepository());
+
+		public IViewComponentResult Invoke()
+		{
+			var values = cm.GetList(4);
+
+			return View(values);
+		}
+
+
+	}
+}
