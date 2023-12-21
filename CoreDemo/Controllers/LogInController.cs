7ba40a5b982e2132c26358e9eab13d8406de70ca
msayrac <msayracode@gmail.com>
2023-12-21 12:03:19 +0300

    Authorization added

--- CoreDemo/Controllers/LogInController.cs
+++ CoreDemo/Controllers/LogInController.cs
@@ -1,15 +1,33 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using DataAccessLayer.Concrete;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
 namespace CoreDemo.Controllers
 {
 	public class LogInController : Controller
 	{
+		[AllowAnonymous]
 		public IActionResult Index()
 		{
 			return View();
 		}
 
-
-
+		[HttpPost]
+		[AllowAnonymous]
+		public IActionResult Index(Writer p)
+		{
+			Context c = new Context();
+			var dataValue = c.Writers.FirstOrDefault(x=>x.WriterMail==p.WriterMail && x.WriterPassword==p.WriterPassword);
+			if(dataValue!=null)
+			{
+				HttpContext.Session.SetString("username", p.WriterMail);
+				return RedirectToAction("Index", "Writer");
+			}
+			else
+			{
+				return View();
+			}			
+		}
 	}
 }
--- CoreDemo/Program.cs
+++ CoreDemo/Program.cs
@@ -1,7 +1,26 @@
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc.Authorization;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddSession();
+
+
+
+builder.Services.AddMvc(config =>
+{
+	var policy = new AuthorizationPolicyBuilder()
+	.RequireAuthenticatedUser()
+	.Build();
+	config.Filters.Add(new AuthorizeFilter(policy));
+
+});
+
+
+
 
 var app = builder.Build();
 
@@ -15,6 +34,7 @@ if (!app.Environment.IsDevelopment())
 
 app.UseStatusCodePagesWithReExecute("/ErrorPage/Error1", "?code={0}");
 
+app.UseSession();
 
 
 
