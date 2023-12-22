1bfe05ebd72300364ac0f6bb7d7c3024c03d9e12
msayrac <msayracode@gmail.com>
2023-12-22 14:49:55 +0300

    Message and notification component added on the writerlayout

---
+++ CoreDemo/ViewComponents/Writer/WriterMessageNotification.cs
@@ -0,0 +1,18 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Identity.Client;
+
+namespace CoreDemo.ViewComponents.Writer
+{
+	public class WriterMessageNotification : ViewComponent
+	{
+		public IViewComponentResult Invoke()
+		{
+
+			return View();
+		}
+
+
+	}
+}
