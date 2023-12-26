87b0e37cabdb07e8234ec933c4dd64a009682200
msayrac <msayracode@gmail.com>
2023-12-26 13:31:55 +0300

    Trigger studied

--- EntityLayer/Concrete/Comment.cs
+++ EntityLayer/Concrete/Comment.cs
@@ -15,8 +15,9 @@ namespace EntityLayer.Concrete
         public string CommentTitle { get; set; }
         public string CommentContent { get; set; }
         public DateTime CommentDate { get; set; }
-        public bool CommentStatus { get; set; }
 
+        public int BlogScore { get; set; }
+        public bool CommentStatus { get; set; }
         public int BlogID { get; set; }
         public Blog Blog { get; set; }
     }
