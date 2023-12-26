4ee42446e205b5994c975cf6614617cf7c236e65
msayrac <msayracode@gmail.com>
2023-12-26 13:50:33 +0300

    WriterAboutOnDashboard created

--- CoreDemo/Controllers/DashboardController.cs
+++ CoreDemo/Controllers/DashboardController.cs
@@ -1,9 +1,15 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
 namespace CoreDemo.Controllers
 {
 	public class DashboardController : Controller
 	{
+		BlogManager cm = new BlogManager(new EfBlogRepository());
+
+		[AllowAnonymous]
 		public IActionResult Index()
 		{
 			return View();
