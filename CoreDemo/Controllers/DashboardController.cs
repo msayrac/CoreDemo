3d9c5fd6047284d1b2bcd683a6809dec04075eeb
msayrac <msayracode@gmail.com>
2023-12-26 21:08:37 +0300

    Writer profile edit page

--- CoreDemo/Controllers/DashboardController.cs
+++ CoreDemo/Controllers/DashboardController.cs
@@ -1,4 +1,5 @@
 ﻿using BusinessLayer.Concrete;
+using DataAccessLayer.Concrete;
 using DataAccessLayer.EntityFramework;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
@@ -12,7 +13,18 @@ namespace CoreDemo.Controllers
 		[AllowAnonymous]
 		public IActionResult Index()
 		{
+			Context c = new Context();
+			ViewBag.v1 = c.Blogs.Count().ToString();
+			ViewBag.v2 =c.Blogs.Where(x=>x.WriterID==1).Count();
+			ViewBag.v3 = c.Categories.Count();
+
+
 			return View();
 		}
+
+
+
+
+
 	}
 }
