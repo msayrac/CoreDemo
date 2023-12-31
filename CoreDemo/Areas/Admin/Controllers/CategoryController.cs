9db1c4a95ba3f3fdc9271a168a0c076ad89bf11f
msayrac <msayracode@gmail.com>
2023-12-31 18:44:18 +0300

    Paged added

--- CoreDemo/Areas/Admin/Controllers/CategoryController.cs
+++ CoreDemo/Areas/Admin/Controllers/CategoryController.cs
@@ -1,6 +1,8 @@
 ﻿using BusinessLayer.Concrete;
 using DataAccessLayer.EntityFramework;
 using Microsoft.AspNetCore.Mvc;
+using X.PagedList;
+
 
 namespace CoreDemo.Areas.Admin.Controllers
 {
@@ -9,9 +11,9 @@ namespace CoreDemo.Areas.Admin.Controllers
 	{
 		CategoryManager cm = new CategoryManager(new EfCategoryRepository());
 
-		public IActionResult Index()
+		public IActionResult Index(int page=1)
 		{
-			var values = cm.GetList();
+			var values = cm.GetList().ToPagedList(page,3);
 			return View(values);
 		}
 	}
