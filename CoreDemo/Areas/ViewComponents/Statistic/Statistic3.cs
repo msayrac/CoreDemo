b97231c8f47410bd893526a35d5c63feea07cb67
msayrac <msayracode@gmail.com>
2024-01-02 09:43:43 +0300

    Statiscit 2-4 added

---
+++ CoreDemo/Areas/ViewComponents/Statistic/Statistic3.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CoreDemo.Areas.ViewComponents.Statistic
+{
+	public class Statistic3: ViewComponent
+	{
+		public IViewComponentResult Invoke()
+		{
+			return View();
+		}
+
+
+
+
+	}
+}
