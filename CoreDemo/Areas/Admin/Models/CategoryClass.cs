09491676a17390660544abeebf60ad41f7027009
msayrac <msayracode@gmail.com>
2024-01-02 11:17:09 +0300

    charts added in admin panel

---
+++ CoreDemo/Areas/Admin/Models/CategoryClass.cs
@@ -0,0 +1,14 @@
+﻿namespace CoreDemo.Areas.Admin.Models
+{
+	public class CategoryClass
+	{
+        public int categorycount { get; set; }
+        public string categoryname { get; set; }
+
+
+
+
+
+
+    }
+}
