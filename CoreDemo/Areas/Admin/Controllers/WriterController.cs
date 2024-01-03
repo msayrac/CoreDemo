fce44ef1767dd11f7b4b3aafb7bdf419c42873a5
msayrac <msayracode@gmail.com>
2024-01-03 12:09:08 +0300

    writer called by id

--- CoreDemo/Areas/Admin/Controllers/WriterController.cs
+++ CoreDemo/Areas/Admin/Controllers/WriterController.cs
@@ -20,6 +20,18 @@ namespace CoreDemo.Areas.Admin.Controllers
             return Json(JsonWriters);
         }
 
+        public IActionResult GetWriterByID(int writerid)
+        {
+            var findWriter = writers.FirstOrDefault(x => x.Id == writerid);
+            var jsonWriters =JsonConvert.SerializeObject(findWriter);
+            return Json(jsonWriters);
+
+
+		}
+
+
+
+
         public static List<WriterClass> writers = new List<WriterClass>
         {
             new WriterClass
