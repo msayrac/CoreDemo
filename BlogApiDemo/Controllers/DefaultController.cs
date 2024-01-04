73a662b07211402b0787bd6fdc22ed1d3c037138
msayrac <msayracode@gmail.com>
2024-01-04 11:57:03 +0300

    BlogDemoApi Added

---
+++ BlogApiDemo/Controllers/DefaultController.cs
@@ -0,0 +1,22 @@
+﻿using BlogApiDemo.DataAccessLayer;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlogApiDemo.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class DefaultController : ControllerBase
+	{
+		[HttpGet]
+		public IActionResult EmployeeList()
+		{
+			using var c = new Context();
+
+			var values = c.Employees.ToList();
+
+			return Ok(values);
+
+		}
+	}
+}
---
+++ BlogApiDemo/DataAccessLayer/Context.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BlogApiDemo.DataAccessLayer
+{
+	public class Context: DbContext
+	{
+		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+		{
+			optionsBuilder.UseSqlServer("server=.;database=CoreBlogApiDb; integrated security=true;TrustServerCertificate=True;");
+		}
+
+        public DbSet<Employee> Employees { get; set; }
+
+
+
+
+    }
+}
