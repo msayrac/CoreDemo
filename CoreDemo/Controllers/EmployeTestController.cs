c109ae690e5f0a556c1a85c1c063070905212590
msayrac <msayracode@gmail.com>
2024-01-06 20:21:08 +0300

    api delete and update completed

--- CoreDemo/Controllers/EmployeTestController.cs
+++ CoreDemo/Controllers/EmployeTestController.cs
@@ -4,48 +4,100 @@ using System.Text;
 
 namespace CoreDemo.Controllers
 {
-	public class EmployeTestController : Controller
-	{
-		public async Task<IActionResult> Index()
-		{
-			var httpClient = new HttpClient();
-			var responseMessage = await httpClient.GetAsync("https://localhost:44309/api/Default");
-			var jsonString = await responseMessage.Content.ReadAsStringAsync();
+    public class EmployeTestController : Controller
+    {
+        public async Task<IActionResult> Index()
+        {
+            var httpClient = new HttpClient();
+            var responseMessage = await httpClient.GetAsync("https://localhost:44309/api/Default");
+            var jsonString = await responseMessage.Content.ReadAsStringAsync();
 
-			var values = JsonConvert.DeserializeObject<List<Class2>>(jsonString);
+            var values = JsonConvert.DeserializeObject<List<Class2>>(jsonString);
 
-			return View(values);
-		}
+            return View(values);
+        }
 
-		[HttpGet]
-		public IActionResult AddEmployee()
-		{
-			return View();
-		}
+        [HttpGet]
+        public IActionResult AddEmployee()
+        {
+            return View();
+        }
 
-		[HttpPost]
-		public async Task<IActionResult> AddEmployee(Class2 p)
-		{
-			var httpClient = new HttpClient();
-			var jsonEmployee = JsonConvert.SerializeObject(p);
+        [HttpPost]
+        public async Task<IActionResult> AddEmployee(Class2 p)
+        {
+            var httpClient = new HttpClient();
+            var jsonEmployee = JsonConvert.SerializeObject(p);
 
-			StringContent content = new StringContent(jsonEmployee, Encoding.UTF8, "application/json");
-			var responseMessage = await httpClient.PostAsync("https://localhost:44309/api/Default", content);
+            StringContent content = new StringContent(jsonEmployee, Encoding.UTF8, "application/json");
+            var responseMessage = await httpClient.PostAsync("https://localhost:44309/api/Default", content);
 
-			if (responseMessage.IsSuccessStatusCode)
-			{
-				return RedirectToAction("Index");
-			}
-			return View(p);
-		}
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+            return View(p);
+        }
 
-	}
+        [HttpGet]
+        public async Task<IActionResult> EditEmployee(int id)
+        {
+            var httpClient = new HttpClient();
+            var responseMessage = await httpClient.GetAsync("https://localhost:44309/api/Default/" + id);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonEmployee = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<Class2>(jsonEmployee);
+                return View(values);
+            }
+            else
+            {
+                return RedirectToAction("Index");
 
-	public class Class2
-	{
-		public int ID { get; set; }
-		public string Name { get; set; }
+            }
 
-	}
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> EditEmployee(Class2 p)
+        {
+            var httpClient = new HttpClient();
+            var jsonEmployee = JsonConvert.SerializeObject(p);
+            var content = new StringContent(jsonEmployee, Encoding.UTF8, "application/json");
+            var responseMessage = await httpClient.PutAsync("https://localhost:44309/api/Default/", content);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+            return View(p);
+
+        }
+
+        public async Task<IActionResult> DeleteEmployee(int id)
+        {
+            var httpClient = new HttpClient();
+            var responseMessage = await httpClient.DeleteAsync("https://localhost:44309/api/Default/" + id);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+
+
+
+
+    }
+
+
+
+
+    public class Class2
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+
+    }
+
 }
-
