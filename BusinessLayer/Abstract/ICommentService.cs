3cd3792139bf916d37ca2ef4e889376b6eeb8483
msayrac <msayracode@gmail.com>
2024-01-13 20:55:46 +0300

    Admin/AdminComment Index added

--- BusinessLayer/Abstract/ICommentService.cs
+++ BusinessLayer/Abstract/ICommentService.cs
@@ -10,12 +10,9 @@ namespace BusinessLayer.Abstract
 	public interface ICommentService
 	{
 		void CommentAdd(Comment comment);
-
-		//void CommentDelete(Comment comment);
-		//void CommentUpdate(Comment comment);
 		List<Comment> GetList(int id);
+		List<Comment> GetCommentWithBlog();
 
-		//Comment GetById(int id);
 
 
 	}
---
+++ CoreDemo/Areas/Admin/Controllers/AdminCommentController.cs
@@ -0,0 +1,20 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreDemo.Areas.Admin.Controllers
+{
+	[Area("Admin")]
+	[AllowAnonymous]
+	public class AdminCommentController : Controller
+	{		
+		CommentManager commentManager = new CommentManager(new EfCommentRepository());		
+
+		public IActionResult Index()
+		{
+			var values = commentManager.GetCommentWithBlog();
+			return View(values);
+		}
+	}
+}
