dd68d50c92fe45d2134ac48da735588e52125eac
msayrac <msayracode@gmail.com>
2024-01-16 13:18:58 +0300

    Jason Web Token

---
+++ jwt_core_proje_kampi/Controllers/Default.cs
@@ -0,0 +1,31 @@
+﻿using jwt_core_proje_kampi.Dal;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace jwt_core_proje_kampi.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class Default : ControllerBase
+	{
+		[HttpGet("[action]")]
+		public IActionResult Login()
+		{
+			return Created("", new BuildToken().CreateToken());
+		}
+
+		[Authorize]
+		[HttpGet("[action]")]
+		public IActionResult Page1()
+		{
+
+			return Ok("Sayfa 1 İçin Giriş Başarılı");
+		}
+
+
+
+
+
+	}
+}
